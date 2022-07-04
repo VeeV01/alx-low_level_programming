cee969d0f73b4ab295dadd7fa268399763c0a763
VeeV01 <ezigbovo@gmail.com>
2022-07-03 18:39:56 -0700

    The success combination in business is: Do what you do better... and: do more of what you do

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d, p, q;
+
+	for (d = '0'; d < '8'; d++)
+	{
+	for (p = d + 1; p <= '8'; p++)
+	{
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+
+	if (d == '7' && p == '8' && q == '9')
+	continue;
+
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
