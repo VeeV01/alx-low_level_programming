d1ad7545f1629c467fbf9e8abe08a48f9553e4f3
VeeV01 <ezigbovo@gmail.com>
2022-07-11 16:44:02 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,6 +18,7 @@ void print_to_98(int n)
 	if (n == 98)
 	continue;
 	printf(",");
+	printf(" ");
 	}
 	printf("\n");
 	}
@@ -29,6 +30,7 @@ void print_to_98(int n)
 	if (n == 98)
 	continue;
 	printf(",");
+	printf(" ");
 	}
 	printf("\n");
 	}
